5271984df6cf0f135b8697cefd0cb2aa245b9019
dmitry-strungar <dmitry.strungar@gmail.com>
2026-02-24 13:46:51 +0300

    Create retention_analysis.sql

---
+++ sql/retention_analysis.sql
@@ -0,0 +1,34 @@
+-- Day 1 Retention Analysis
+-- Product: WID Digital Wardrobe
+
+
+WITH first_activity AS (
+    SELECT 
+        user_id,
+        MIN(event_date) AS first_date
+    FROM events
+    WHERE event_name = 'activity'
+    GROUP BY user_id
+),
+
+next_day_activity AS (
+    SELECT DISTINCT e.user_id
+    FROM events e
+    JOIN first_activity f 
+        ON e.user_id = f.user_id
+    WHERE e.event_name = 'activity'
+      AND e.event_date = f.first_date + INTERVAL '1 day'
+)
+
+SELECT
+    COUNT(DISTINCT f.user_id) AS total_users,
+    COUNT(DISTINCT n.user_id) AS returned_next_day,
+
+    ROUND(
+        COUNT(DISTINCT n.user_id) * 1.0
+        / NULLIF(COUNT(DISTINCT f.user_id), 0),
+    3) AS day_1_retention_rate
+
+FROM first_activity f
+LEFT JOIN next_day_activity n 
+    ON f.user_id = n.user_id
