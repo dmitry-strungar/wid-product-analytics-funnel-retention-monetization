450abb07dfde47f07eb505b313b88b0afa572cc5
dmitry-strungar <dmitry.strungar@gmail.com>
2026-02-24 14:16:30 +0300

    Create ab_test_analysis.sql

---
+++ sql/ab_test_analysis.sql
@@ -0,0 +1,26 @@
+-- A/B Test Analysis: Monetization Scenario
+-- Product: WID Digital Wardrobe
+
+WITH experiment_data AS (
+    SELECT
+        user_id,
+        group_name,       -- 'A' or 'B'
+        subscribed,       -- 1 = subscribed
+        revenue
+    FROM ab_test_dataset
+),
+
+aggregated AS (
+    SELECT
+        group_name,
+        COUNT(*) AS users,
+        SUM(subscribed) AS subscriptions,
+        SUM(revenue) AS total_revenue,
+        ROUND(SUM(subscribed) * 1.0 / COUNT(*), 4) AS conversion_rate,
+        ROUND(SUM(revenue) * 1.0 / COUNT(*), 2) AS arpu
+    FROM experiment_data
+    GROUP BY group_name
+)
+
+SELECT *
+FROM aggregated
