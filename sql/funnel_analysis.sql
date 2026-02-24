5573d077931597652fd6be40d8d50767f93edec6
dmitry-strungar <dmitry.strungar@gmail.com>
2026-02-24 13:42:17 +0300

    Update funnel_analysis.sql

--- sql/funnel_analysis.sql
+++ sql/funnel_analysis.sql
@@ -33,8 +33,24 @@ SELECT
     COUNT(DISTINCT v.user_id) AS visits_users,
     COUNT(DISTINCT r.user_id) AS registrations_users,
     COUNT(DISTINCT a.user_id) AS active_users,
-    COUNT(DISTINCT s.user_id) AS subscribed_users
+    COUNT(DISTINCT s.user_id) AS subscribed_users,
+
+    ROUND(
+        COUNT(DISTINCT r.user_id) * 1.0 
+        / NULLIF(COUNT(DISTINCT v.user_id), 0), 
+    3) AS visit_to_registration_rate,
+
+    ROUND(
+        COUNT(DISTINCT a.user_id) * 1.0 
+        / NULLIF(COUNT(DISTINCT r.user_id), 0), 
+    3) AS registration_to_activity_rate,
+
+    ROUND(
+        COUNT(DISTINCT s.user_id) * 1.0 
+        / NULLIF(COUNT(DISTINCT a.user_id), 0), 
+    3) AS activity_to_subscription_rate
+
 FROM visits v
 LEFT JOIN registrations r ON v.user_id = r.user_id
 LEFT JOIN activity a ON v.user_id = a.user_id
-LEFT JOIN subscriptions_stage s ON v.user_id = s.user_id;
+LEFT JOIN subscriptions_stage s ON v.user_id = s.user_id
