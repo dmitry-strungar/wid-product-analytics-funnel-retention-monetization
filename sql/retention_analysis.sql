643a63efc0662a2edb3c77c1802c8655896f5421
Дмитрий Струнгарь <dmitry.strungar@gmail.com>
2026-03-24 12:17:13 +0300

    Update retention_analysis.sql

--- sql/retention_analysis.sql
+++ sql/retention_analysis.sql
@@ -2,33 +2,33 @@
 -- Продукт: Цифровой гардероб WID
 
 
-WITH first_activity AS (
-    SELECT 
+with first_activity as (
+    select
         user_id,
-        MIN(event_date) AS first_date
-    FROM events
-    WHERE event_name = 'activity'
-    GROUP BY user_id
+        MIN(event_date) as first_date
+    from events
+    where event_name = 'activity'
+    group by user_id
 ),
 
-next_day_activity AS (
-    SELECT DISTINCT e.user_id
-    FROM events e
-    JOIN first_activity f 
-        ON e.user_id = f.user_id
-    WHERE e.event_name = 'activity'
-      AND e.event_date = f.first_date + INTERVAL '1 day'
+next_day_activity as (
+    select distinct e.user_id
+    from events e
+    join first_activity f 
+        on e.user_id = f.user_id
+    where e.event_name = 'activity'
+      and e.event_date = f.first_date + INTERVAL '1 day'
 )
 
-SELECT
-    COUNT(DISTINCT f.user_id) AS total_users,
-    COUNT(DISTINCT n.user_id) AS returned_next_day,
+select
+    count(distinct f.user_id) as total_users,
+    count(distinct n.user_id) as returned_next_day,
 
-    ROUND(
-        COUNT(DISTINCT n.user_id) * 1.0
-        / NULLIF(COUNT(DISTINCT f.user_id), 0),
-    3) AS day_1_retention_rate
+    round(
+        count(distinct n.user_id) * 1.0
+        / nullif(count(distinct f.user_id), 0),
+    3) as day_1_retention_rate
 
-FROM first_activity f
-LEFT JOIN next_day_activity n 
-    ON f.user_id = n.user_id
+from first_activity f
+left join next_day_activity n 
+    on f.user_id = n.user_id
